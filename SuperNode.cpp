83914a1c91180566ce0e5685c446845764f4810f
abuzomol <muzamelmahgoub@gmail.com>
2020-01-31 20:20:38 -1000

    Added tree class

--- SuperNode.cpp
+++ SuperNode.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "SuperNode.h"
+SuperNode::SuperNode(const std::vector<double>& val): val(val) {}
+
+const std::vector<double>& SuperNode::getVal() const { return val; }
+void SuperNode::setVal(const std::vector<double>& val) { SuperNode::val = val; }
+Node* SuperNode::getLeftSemiLines() const { return leftSemiLines; }
+void SuperNode::setLeftSemiLines(Node* leftSemiLines)
+{
+  SuperNode::leftSemiLines = leftSemiLines;
+}
+Node* SuperNode::getRightSemiLines() const { return rightSemiLines; }
+void SuperNode::setRightSemiLines(Node* rightSemiLines)
+{
+  SuperNode::rightSemiLines = rightSemiLines;
+}
+MiddleNode* SuperNode::getMiddle() const { return middle; }
+void SuperNode::setMiddle(MiddleNode* middle) { SuperNode::middle = middle; }
--- SuperNode.h
+++ SuperNode.h
@@ -4,7 +4,34 @@
 
 #ifndef POINT_LOC_B_TREE_SUPERNODE_H
 #define POINT_LOC_B_TREE_SUPERNODE_H
+#include <iostream>
+#include <vector>
 
-class SuperNode {};
+#include "MiddleNode.h"
+#include "Node.h"
+#include "MiddleNode.h"
+#include "Node.h"
+
+class SuperNode
+{
+ private:
+  std::vector<double> val;
+  Node *leftSemiLines ;
+  Node *rightSemiLines;
+  MiddleNode *middle;
+
+ public:
+  SuperNode(const std::vector<double>& val);
+  const std::vector<double>& getVal() const;
+  void setVal(const std::vector<double>& val);
+
+  Node* getLeftSemiLines() const;
+  void setLeftSemiLines(Node* leftSemiLines);
+  Node* getRightSemiLines() const;
+  void setRightSemiLines(Node* rightSemiLines);
+  MiddleNode* getMiddle() const;
+  void setMiddle(MiddleNode* middle);
+};
+
 
 #endif // POINT_LOC_B_TREE_SUPERNODE_H
